aea438b931899bef23301770367f025e4784b020
Steve Gury <sgury@netflix.com>
2015-09-26 22:58:07 -0700

    Fist commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Qiro Logo
+
+This project generate the qiro logo.
+
+![The qiro logo](https://raw.githubusercontent.com/qiro/qiro-logo/master/qiro-logo_256x256.jpg)
+
+The logo is based on an image generated from a fractal of the Julia Set, the complex function used
+ is:
+  `f(x + iy) = f(z) = z^2 - 0.2`.
+
+This fractal is often named "Glynn Tree", named after Earl Glynn who was one of the first to 
+generate images based on the previous formula.
+See the [GlynnTree.java](https://raw.githubusercontent.com/qiro/qiro-logo/master/src/main/java/io/qiro/logo/GlynnTree.java) 
+file for more details about the computation. There are two post processing:
+
+- the surrounding dark color is removed by thresholding
+- the grey color of the tree is accentuated (via a square function)
+
+## Generate the logo
+
+Simply run `./gradlew run` to generate a 2048x2048px logo at the root of the project.
+The image is PNG encoded with the name `qiro-logo_2048x2048.png`.
+
+To resize the image, we recommand using imagemagick, e.g.:
+`convert qiro-logo_2048x2048.png -resize 512x512 qiro-logo_512x512.jpg`
+
+## LICENSE
+
+Copyright 2015 Netflix, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+<http://www.apache.org/licenses/LICENSE-2.0>
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ src/main/java/io/qiro/logo/GlynnTree.java
@@ -0,0 +1,138 @@
+package io.qiro.logo;
+
+import java.awt.image.BufferedImage;
+import java.awt.Color;
+import java.io.File;
+import java.io.IOException;
+import javax.swing.JFrame;
+import javax.imageio.ImageIO;
+import org.apache.commons.math3.complex.Complex;
+
+public class GlynnTree {
+
+    private static double julia(Complex zz, int maxIteration) {
+        int i = 0;
+        Complex z = zz;
+        while (i < maxIteration && z.abs() < 1.0) {
+            z = z.pow(1.5).subtract(0.2);
+            i += 1;
+        }
+        return i;
+    }
+
+    private static BufferedImage generate(
+        double xcenter,
+        double xrange,
+        double ycenter,
+        double yrange,
+        int maxIteration,
+        double whiteThreshold,
+        int size
+    ) {
+        BufferedImage image = new BufferedImage(size, size, BufferedImage.TYPE_INT_RGB);
+
+        double xmin = xcenter - xrange / 2;
+        double xmax = xcenter + xrange / 2;
+        double ymin = ycenter - yrange / 2;
+        double ymax = ycenter + yrange / 2;
+
+        double[][] buffer = new double[size][size];
+        double min = Double.MAX_VALUE;
+        double max = Double.MIN_VALUE;
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                double x = (double) i / size * xrange + xmin;
+                double y = ymax - (double) j / size * yrange;
+                double z = julia(new Complex(-x, y), maxIteration);
+                buffer[i][j] = z;
+                min = Math.min(min, z);
+                max = Math.max(max, z);
+            }
+        }
+
+        double range = max - min;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                double z = buffer[i][j];
+                z = (z - min) / range;
+                if (z < whiteThreshold) {
+                    z = 1; // replace the surrounding black by white color
+                }
+                z = z * z;
+                int rgb = (int) (255 * z);
+                image.setRGB(size - j - 1, size - i - 1, new Color(rgb, rgb, rgb).getRGB());
+            }
+        }
+
+        return image;
+    }
+
+    public static void show(
+        double xcenter,
+        double xrange,
+        double ycenter,
+        double yrange,
+        int maxIteration,
+        double whiteThreshold,
+        int size
+    ) throws IOException {
+        BufferedImage image = generate(
+            xcenter, xrange,
+            ycenter, yrange,
+            maxIteration,
+            whiteThreshold,
+            size);
+
+        JFrame f = new JFrame("Julia Example") {
+            @Override
+            public void paint(java.awt.Graphics g) {
+                g.drawImage(image, 0, 0, null);
+            }
+        };
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setSize(size, size);
+        f.repaint();
+        f.setVisible(true);
+    }
+
+    public static void generateImage(
+        double xcenter,
+        double xrange,
+        double ycenter,
+        double yrange,
+        int maxIteration,
+        double whiteThreshold,
+        int size,
+        File output
+    ) throws IOException {
+        BufferedImage image = generate(
+            xcenter, xrange,
+            ycenter, yrange,
+            maxIteration,
+            whiteThreshold,
+            size);
+        ImageIO.write(image, "PNG", output);
+    }
+
+    public static void main(String[] args) throws IOException {
+        double xcenter = 0.54;
+        double xrange = 0.35;
+        double ycenter = 0.0;
+        double yrange = 0.4;
+        int maxIteration = 105;
+        double whiteThreshold = 0.35;
+        int size = 2048;
+
+        //GlynnTree.show(xcenter, xrange, ycenter, yrange, maxIteration, whiteThreshold, 1024);
+        File output = new File("qiro-logo_" + size + "x" + size + ".png");
+        GlynnTree.generateImage(
+            xcenter, xrange,
+            ycenter, yrange,
+            maxIteration,
+            whiteThreshold,
+            size,
+            output
+        );
+    }
+}
